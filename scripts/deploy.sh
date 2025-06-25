bc2ef5bd4df2adea437c5f6a38825fdcdd062b90
sa1347 <sa134759@gmail.com>
2025-06-25 18:49:09 +0900

    git commit -m "2nd commit"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#BUILD_JAR=$(ls /home/ubuntu/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
+echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
 
-echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
-#DEPLOY_PATH=/home/ec2-user/
+echo ">>> build 파일 복사" >> /home/ec2-user/deploy.log
+DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_JAR $DEPLOY_PATH
 
-#echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
+echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ec2-user/deploy.log
 sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-#echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
-#nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/deploy_err.log &
+echo ">>> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
+# 터미널 세션이 끊겨도 프로세스가 종료되지 않게 함
