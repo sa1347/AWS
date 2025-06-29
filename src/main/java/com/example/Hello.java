7b3ec6042f9866da48b7c314cd3cd9a1aaefad4b
sa1347 <73269124+sa1347@users.noreply.github.com>
2025-06-29 20:28:50 +0900

    Create Hello.java

---
+++ src/main/java/com/example/Hello.java
@@ -0,0 +1,9 @@
+cat > src/main/java/com/example/Hello.java <<EOF
+package com.example;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+EOF
